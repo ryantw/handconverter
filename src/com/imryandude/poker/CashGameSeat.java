0e00eeb8ca6e4f6aa8790a919b14f01dace77041
ryan <ryantwalk@gmail.com>
2017-04-12 10:07:57 -0400

    create random player names, except for hero

--- src/com/imryandude/poker/CashGameSeat.java
+++ src/com/imryandude/poker/CashGameSeat.java
@@ -1,22 +1,56 @@
 package com.imryandude.poker;
 
+import java.util.Random;
+
 /**
  * Created by ryan on 4/4/17.
  */
 public class CashGameSeat {
-    private int handNumber;
+    private String handNumber;
     private int seatNumber;
     private String userName;
+    private String tablePosition;
     private float userMoney;
     boolean isHero = false;
     boolean dealer = false;
     boolean isNewPlayer = false;
+    private static Random random = new Random();
+    private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public CashGameSeat(int seatNumber, String userName, float userMoney){
+    public CashGameSeat(){
+
+    }
+
+    public CashGameSeat(String handNumber, int seatNumber, String tablePosition, float userMoney, boolean isHero, boolean isNew){
+        this.handNumber = handNumber;
         this.seatNumber = seatNumber;
-        this.userName = userName;
+        this.tablePosition = tablePosition;
         this.userMoney = userMoney;
-        this.isNewPlayer = true;
+        if(isNew && !isHero)
+            this.userName = generateUserName();
+        if(isHero)
+            this.userName = "Hero";
+
+        System.out.println(userName);
+    }
+
+    private String generateUserName(){
+        StringBuilder userName = new StringBuilder("PLR_");
+        for(int i = 0; i < 7; i++)
+            userName.append(randomInt());
+
+        for(int i = 0; i < 2; i++)
+            userName.append(randomChar());
+
+        return userName.toString();
+    }
+
+    public int randomInt(){
+        return random.nextInt(10);
+    }
+
+    public char randomChar(){
+        return alphabet.charAt(random.nextInt(alphabet.length()));
     }
 
     public int getSeatNumber() {
